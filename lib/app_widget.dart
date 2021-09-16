ea08f554593de0151d3f787a8d6fbfec6d23acac
maryhanakuhn <maryhanakuhn@gmail.com>
2021-09-15 22:50:23 -0300

    barcode_scanner

--- lib/app_widget.dart
+++ lib/app_widget.dart
@@ -1,4 +1,7 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
+import 'package:payapp/modules/barcode_scanner/barcode_scanner_page.dart';
+import 'package:payapp/modules/insert_boleto/insert_boleto_page.dart';
 
 import 'modules/home/home_page.dart';
 import 'modules/login/login_page.dart';
@@ -10,16 +13,25 @@ void main() {
 }
 
 class AppWidget extends StatelessWidget {
+  AppWidget() {
+    SystemChrome.setPreferredOrientations([
+      DeviceOrientation.portraitDown,
+      DeviceOrientation.portraitUp,
+    ]);
+  }
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
         title: "PayApp",
-        theme: ThemeData(primaryColor: AppColors.primary),
+        theme: ThemeData(
+            primarySwatch: Colors.green, primaryColor: AppColors.primary),
         initialRoute: "/splash",
         routes: {
           "/splash": (context) => SplashPage(),
           "/home": (context) => HomePage(),
           "/login": (context) => LoginPage(),
+          "/barcode_scanner": (context) => BarcodeScannerPage(),
+          "/insert_boleto": (context) => InsertBoletoPage(),
         });
   }
 }
