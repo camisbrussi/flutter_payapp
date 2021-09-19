d408d154f5beba18305e6fb2b40836a2f69c52f2
maryhanakuhn <maryhanakuhn@gmail.com>
2021-09-19 14:44:06 -0300

    fix barcode scanner

--- lib/modules/barcode_scanner/barcode_scanner_page.dart
+++ lib/modules/barcode_scanner/barcode_scanner_page.dart
@@ -49,7 +49,7 @@ class _BarcodeScannerPageState extends State<BarcodeScannerPage> {
               builder: (_, status, __) {
                 if (status.showCamera) {
                   return Container(
-                    child: status.cameraController!.buildPreview(),
+                    child: controller.cameraController!.buildPreview(),
                   );
                 } else {
                   return Container();
@@ -78,7 +78,9 @@ class _BarcodeScannerPageState extends State<BarcodeScannerPage> {
                 ]),
                 bottomNavigationBar: SetLabelButtons(
                     primaryLabel: "Inserir código do boleto",
-                    primaryOnPressed: () {},
+                    primaryOnPressed: () {
+                      Navigator.pushReplacementNamed(context, "/insert_boleto");
+                    },
                     secondaryLabel: "Adicionar da galeria",
                     secondaryOnPressed: () {})),
           ),
@@ -92,10 +94,12 @@ class _BarcodeScannerPageState extends State<BarcodeScannerPage> {
                         "Tente escanear novamente ou digite o código do seu boleto",
                     primaryLabel: "Escanear novamente",
                     primaryOnPressed: () {
-                      controller.getAvailableCameras();
+                      controller.scanWithCamera();
                     },
                     secondaryLabel: "Digitar código",
-                    secondaryOnPressed: () {},
+                    secondaryOnPressed: () {
+                      Navigator.pushReplacementNamed(context, "/insert_boleto");
+                    },
                   );
                 } else {
                   return Container();
